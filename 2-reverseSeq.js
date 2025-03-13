86af66dcd3777e5f6e09845ebc4ccc13f99c799b
VICKYGracie <psychologycoding@gmail.com>
2025-03-13 11:24:49 +0700

    challenge reverset sequence 2/3/2025

---
+++ 2-reverseSeq.js
@@ -0,0 +1,29 @@
+// Coding Challenge Day 2 bulan 3 
+// day 2/3/2025
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
+
+// Cara yang pertama 
+// const reverseSeq = (n) => {
+//     const result = [];
+//     for (let i = n; i >= 1; i--) { // Mengunakan Looping
+//         result.push(i);
+//     }
+//     return result;
+//   };
+
+
+// cara yang kedua
+// const reverseSeq = (n) => {
+//     // return [...Array(n)];
+//     return Array(n).fill().map((el, i) => i + 1) // bisa mengunakan 2 cara
+//     .reverse();
+//   }
+
+// untuk cara singkatnya dari yang ke 2 dan menjadi 1 baris
+// const reverseSeq = n => [...Array(n)].map((el, i) => i + 1).reverse();
+
+//menggunakan cara yanga lain juga bisa 
+const reverseSeq = n => [...Array(n)].map((el, i) => n - 1);
+
+
+console.log(reverseSeq(5)); //[ 5, 4, 3, 2, 1]
